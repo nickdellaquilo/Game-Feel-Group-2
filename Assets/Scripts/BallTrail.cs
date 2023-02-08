da7150c993081ba4b1029eb09c85d132dec2697c
Courtney Battieste <cbb389@nyu.edu>
2023-02-07 23:57:24 -0500

    ball trail attempt 1

--- Assets/Scripts/BallTrail.cs
+++ Assets/Scripts/BallTrail.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BallTrail : MonoBehaviour
 {
+    private TrailRenderer trail;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        trail = GetComponent<TrailRenderer>();   
     }
 
     // Update is called once per frame
